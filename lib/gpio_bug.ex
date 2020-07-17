4b97fc600ca92402112937e4907331e3651df4fe
alanj853 <alanjackson.nuig@gmail.com>
2020-07-17 14:10:09 +0100

    first commit

---
+++ lib/gpio_bug.ex
@@ -0,0 +1,18 @@
+defmodule GpioBug do
+  @moduledoc """
+  Documentation for `GpioBug`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GpioBug.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gpio_bug_test.exs
@@ -0,0 +1,8 @@
+defmodule GpioBugTest do
+  use ExUnit.Case
+  doctest GpioBug
+
+  test "greets the world" do
+    assert GpioBug.hello() == :world
+  end
+end
